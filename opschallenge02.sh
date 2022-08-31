0211b63ce22ede89cc8317a4ac4425f7c5508d74
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-08-30 19:32:17 -0500

    Create opschallenge02.sh

---
+++ opschallenge02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script: Ops 301 Class 02 Ops Challenge Solution
+# Author: Justin Buffington
+# Date of latest revision: 8/30/2022
+# Purpose: Copies log to working directory and appends the current date and time to the filename
+
+# Main
+
+day=$(date +%D%T)
+
+cp /var/log/syslog .
+
+mv syslog syslog.$day
+
+# End

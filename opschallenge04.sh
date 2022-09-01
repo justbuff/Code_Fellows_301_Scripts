751376056a3a968460eca5577e1c2234bcca4ef1
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-09-01 13:25:47 -0500

    Update opschallenge04.sh

--- opschallenge04.sh
+++ opschallenge04.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+# Script: Ops 301 Class 04 Ops Challenge Solution
+# Author: Justin Buffington
+# Date of latest revision: 9/1/2022
+# Purpose: Asks user series of questions within a loop
+
+# Main

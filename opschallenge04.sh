831ed2cefe98f2a81222b2857cf54ab067e90aaa
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-09-01 22:33:14 -0500

    Update opschallenge04.sh

--- opschallenge04.sh
+++ opschallenge04.sh
@@ -3,6 +3,27 @@
 # Script: Ops 301 Class 04 Ops Challenge Solution
 # Author: Justin Buffington
 # Date of latest revision: 9/1/2022
-# Purpose: Asks user series of questions within a loop
+# Purpose: Provides a user with a set of options to chose from and executes the selected option
+
+PS3='Please select an option: ' # PS3 is the parameter for the select command
+
+select opt in "Hello World" "Ping self" "IP info" "Exit" # this creates the option menu using the select loop
+do
+    case $opt in
+        "Hello World") # this prints Hello World! to the terminal
+            echo "Hello World!"
+            ;;
+        "Ping self") # this pings the local loopback address
+            ping -c 3 localhost
+            ;;
+        "IP info") # this displays the network adapter info
+            lspci | grep -1 net
+            ;;
+        "Exit") # this exits the loop and stops the script
+            break
+            ;;
+    esac
+    REPLY= # this makes the menu appear after picking an option
+done
 
 # Main

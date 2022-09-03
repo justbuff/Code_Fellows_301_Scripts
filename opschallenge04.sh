7625aae5f296e23b4c215b6132d88198695f527c
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-09-02 23:38:10 -0500

    Update opschallenge04.sh

--- opschallenge04.sh
+++ opschallenge04.sh
@@ -5,6 +5,8 @@
 # Date of latest revision: 9/1/2022
 # Purpose: Provides a user with a set of options to chose from and executes the selected option
 
+# Main
+
 PS3='Please select an option: ' # PS3 is the parameter for the select command
 
 select opt in "Hello World" "Ping self" "IP info" "Exit" # this creates the option menu using the select loop

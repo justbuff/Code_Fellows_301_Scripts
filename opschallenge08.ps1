9ff2ad4895117e684d5a0eba117598fcbbf0c3cd
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-09-09 00:17:24 -0500

    Update and rename opschallenge08.py to opschallenge08.ps1

---
+++ opschallenge08.ps1
@@ -0,0 +1,3 @@
+Import-Module ActiveDirectory
+
+New-ADUser -Name "Franz Ferdinand" -Title "TPS Reporting Lead" -EmailAddress "ferdi@globexpower.com" -Department "TPS Department" -Company "GlobeX USA" -City "Springfield" -State "OR"

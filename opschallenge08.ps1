4d2f0346a032c60c37775a6d69bec97bd2f59905
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-09-09 17:06:13 -0500

    Update opschallenge08.ps1

--- opschallenge08.ps1
+++ opschallenge08.ps1
@@ -1,3 +1,12 @@
+# Script: Ops 301 Class 08 Ops Challenge Solution
+# Author: Justin Buffington
+# Date of latest revision: 9/8/2022
+# Purpose: Creates a new Active Directory User with specific parameters using PowerShell
+
+# Main
+
 Import-Module ActiveDirectory
 
 New-ADUser -Name "Franz Ferdinand" -Title "TPS Reporting Lead" -EmailAddress "ferdi@globexpower.com" -Department "TPS Department" -Company "GlobeX USA" -City "Springfield" -State "OR"
+
+# End

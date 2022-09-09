ba6defe874fcec1eae0424e32ffdf661a20c2938
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-09-09 17:27:23 -0500

    Update and rename opschallenge09.ps1 to opschallenge09.py

---
+++ opschallenge09.py
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script: Ops 301 Class 09 Ops Challenge Solution
+# Author: Justin Buffington
+# Date of latest revision: 9/9/2022
+# Purpose: Prints specific elements from a list
+
+# Main
+
+networkTopology = ['Router0', 'Router1', 'Switch0', 'Switch1', 'Switch2', 'Firewall', 'PC0', 'PC1', 'Server0', 'Server1']
+
+print(networkTopology[3])
+print(networkTopology[5:11])
+networkTopology[6] = 'PC2'
+
+# End

a236bc6ffe53d1ef5d21a93627e69b9e2e2f080a
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-09-12 21:44:39 -0500

    Update opschallenge10.py

--- opschallenge10.py
+++ opschallenge10.py
@@ -1,1 +1,39 @@
+#!/usr/bin/env python3
 
+# Script: Ops 301 Class 10 Ops Challenge Solution
+# Author: Justin Buffington
+# Date of latest revision: 9/10/2022
+# Purpose: Prints logical conditionals
+
+# Assign variables
+a = 5
+b = 7
+
+# Main
+
+if a == b:
+  print("a equals b")
+if a != b:
+  print("a does not equal b")
+if a < b:
+  print("a is less than b")
+if a <= b:
+  print("a is less than or equal to b")
+if a > b:
+  print("a is greater than b")
+if a >= b:
+  print("a is greater than or equal to b")
+
+if a == b:
+  print("a equals b")
+elif a != b:
+  print("a does not equal b")
+
+if a > b:
+  print("a is greater than b")
+elif a < b:
+  print("a is less than b")
+else: 
+  print("a equals b")
+  
+  # End

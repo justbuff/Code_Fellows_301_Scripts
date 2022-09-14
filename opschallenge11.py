a871f3c57618bdb3c3701315943516f7a5e7e474
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-09-13 21:44:50 -0500

    Update opschallenge11.py

--- opschallenge11.py
+++ opschallenge11.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python3
 
+# Script: Ops 301 Class 11 Ops Challenge Solution
+# Author: Justin Buffington
+# Date of latest revision: 9/11/2022
+# Purpose: Opens and manipulates an existing file
+
+# Main
+
+# Creates txt file and adds 3 lines
+
+file = open("newfile.txt", "w") # write mode
+lines = ["This is line 1 \n", "This is line 2 \n", "This is line 3"]
+file.writelines(lines)
+
+# Prints line 1
+
+printline = file.readlines(1)
+print(printline)
+
+# Deletes file
+import os
+os.remove("newfile.txt")
+
+# End
